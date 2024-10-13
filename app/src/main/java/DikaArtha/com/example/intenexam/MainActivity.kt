c29bac718842f0455e6295abe905247632e74151
MSI GAMING <arthdika17@gmail.com>
2024-10-13 12:40:29 +0700

    add

---
+++ app/src/main/java/DikaArtha/com/example/intenexam/MainActivity.kt
@@ -0,0 +1,36 @@
+package DikaArtha.com.example.intenexam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import DikaArtha.com.example.intenexam.databinding.ActivityMainBinding
+import java.util.logging.Logger
+
+class MainActivity : AppCompatActivity() {
+    private val Log = Logger.getLogger(MainActivity::class.java.name)
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.info("onCreate")
+        binding.btnToSecondActivity.setOnClickListener {
+            val intent = Intent(this@MainActivity,
+                SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.info("onPause")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.info("onRestart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.info("onResume")
+    }
+}
---
+++ app/src/main/java/DikaArtha/com/example/intenexam/SecondActivity.kt
@@ -0,0 +1,27 @@
+package DikaArtha.com.example.intenexam
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import DikaArtha.com.example.intenexam.databinding.ActivitySecondBinding
+import java.util.logging.Logger
+class SecondActivity : AppCompatActivity() {
+    private val Log = Logger.getLogger(SecondActivity::class.java.name)
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.info("onCreate")
+        binding.btnExitActivity.setOnClickListener {
+            finish()
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.info("onStart")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.info("onStop")
+    }
+}
